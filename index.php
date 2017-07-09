fa5688a30f214ecd4ec4c0fa51d2469202d0e2ee
flyooo <fly0oo@hotmail.com>
2017-07-09 22:44:08 +0800

    core

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 	require_once('include/config.inc.php');
-	session_start();
 	$ct=isset($_REQUEST['ct']) ? $_REQUEST['ct'] : 'admin';
 	$ac=isset($_REQUEST['ac']) ? $_REQUEST['ac'] : 'index';	
 	if(empty($_SESSION["USERNAME"]))
